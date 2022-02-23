bb47d3cef6db78ad35482eb5f68886f6bbdb5c13
Moyinoluwa Fesobi <moyinoluwafesobi7@gmail.com>
2022-02-23 16:09:34 +0100

    Modify: Navbar creation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import img from '../assets/logo.svg';
+
+const NavbarContainer = styled.section``
+const NavbarWrap = styled.section``
+const Logo = styled.section``
+const Nav = styled.section``
+const NavLink = styled.section``
+const Search = styled.section``
+const SearchWrap = styled.section``
+const Input = styled.section``
+const ButtonContainer = styled.section``
+const Button = styled.section``
+
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar Section</h1>
-    </div>
+    <NavbarContainer>
+      <NavbarWrap>
+        <Logo />
+        <Nav>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/">MARKET</NavLink>
+          <NavLink to="/">TRADE</NavLink>
+          <NavLink to="/">PRICING</NavLink>
+          <NavLink to="/">DOWNLOAD</NavLink>
+          <NavLink to="/">HELP</NavLink>
+        </Nav>
+        <Search>
+          <SearchWrap>
+            <Input />
+          </SearchWrap>
+        </Search>
+        <ButtonContainer>
+          <Button>  SIGN UP </Button>
+          <Button>  LOG IN </Button>
+        </ButtonContainer>
+      </NavbarWrap>
+    </NavbarContainer>
   )
 }
 
